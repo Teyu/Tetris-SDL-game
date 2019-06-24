5e80f031c366f8f989c2e66e1d6f8816de58b3bb
Michaely <j.michaely@pmdtec.com>
2019-06-24 18:11:27 +0200

    simplified integration of SDL for windows

--- cmake/specifySDLpaths.cmake
+++ cmake/specifySDLpaths.cmake
@@ -1,24 +1,12 @@
 find_package(SDL REQUIRED)
 
-if(SDL_FOUND)
-  include_directories(
-    ${SDL_INCLUDE_DIR}
-    ${SDL_INCLUDE_DIR}/SDL
-  )
-  target_link_libraries(Sylwia
-    ${SDL_LIBRARY}
-  )
-endif()
+#if(SDL_FOUND)
+#endif()
 
 if(NOT SDL_LIBRARY)
   message( FATAL_ERROR "SDL_LIBRARY not set. Please specify the path to the SDL library using the SDL_LIBRARY tag.")
 else()
   message( "Found SDL_LIBRARY under ${SDL_LIBRARY}.")
-endif()
-if(NOT SDL_INCLUDE_DIR)
-  message( FATAL_ERROR "SDL_INCLUDE_DIR not set. Please specify the path to the SDL include directoy using the SDL_INCLUDE_DIR tag.")
-else()
-  message( "Found SDL_INCLUDE_DIR under ${SDL_INCLUDE_DIR}." )
 endif()
 
 
